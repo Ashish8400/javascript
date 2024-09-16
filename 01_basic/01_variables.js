7cd205e6176c8f6762bb2089a45a3b836898f16d
Ashish Yadav <128246175+Ashish8400@users.noreply.github.com>
2024-09-16 19:22:31 +0000

    datatype

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,11 +2,14 @@ const accountId = 123456;
 /* this const keyword is use for const value that is not changeable 
 */
 let accountemail = "ashish@gmail.com";
+/* let is way to declare the vrriable. if we can declare the varriable ones then we can not declare with same name of varriable*/
 var accountpassword = "12234242";
 
-/* prefer not use the var keyword because issue in block scope and functional scope */
+/* prefer not use the var keyword because issue in block scope and functional scope and if we can declare ones then also we can delare with same name  another varriable */
 
 accountCity = "Greater noida";
 let accountstate;
 console.log(accountId);
+/* console.log is  fuction that is use in display the message in consolee */
 console.table([accountId, accountemail, accountpassword, accountstate,accountCity]);
+/* if we use console.table it give the output in table form */
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,18 @@
+let name= "ashish"
+let age= 19
+let Isloggedin = true
+let ashhs 
+
+//number -> this type is use for integer type () 2 to the 53)
+//bigint -> this is use for big type of integer data
+//string -> this data type is use for store the sequence of character ( " ") 
+//boolean -> this data type is use for true or false
+//null -> standalone value
+//undefine => this data type is use for declare a variable but doesnot store anything 
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof ashhs)
+
+//typeof is use for check for type of variable 
+//last is symbol type 
