ae007cc7ef04b1e80cc7925fe78d62082d2907f4
Ashish Yadav <128246175+Ashish8400@users.noreply.github.com>
2024-09-17 10:17:38 +0000

    new

--- 01_basic/03_operaterconv.js
+++ 01_basic/03_operaterconv.js
@@ -3,8 +3,8 @@ let name="ashish"
 //string to number conversion =>
 
     let num= Number(name)
-    //console.log(typeof num)
-    //console.log(num)
+    console.log(typeof num)
+    console.log(num)
 
 //its give a NaN output it. means it not a number because we convert string to number 
 
@@ -12,29 +12,29 @@ let name="ashish"
 
     
     let num1 = String(roll)    
-    //console.log(typeof num1)
-    //console.log(num1)
+    console.log(typeof num1)
+    console.log(num1)
 
 //boolean to string =>
     let booleanInnum = true
     let num3 = String(booleanInnum)
-   // console.log(typeof num3) 
-    //console.log(num3)
+    console.log(typeof num3) 
+    console.log(num3)
 
 //string to boolean=>
     let name1 
     //if the variable is undefine then it gives output  is false /and tpe is boolean
 
     let num4 = Boolean(name1);
-    //console.log(num4)
-    //console.log(typeof num4)
+    console.log(num4)
+    console.log(typeof num4)
 
     //if the variable should null then it gives false and type is boolean
 
     let ash = null;
     let ashcheck = Boolean(ash)
-    //console.log(ashcheck)
-    //console.log(typeof ashcheck)
+    console.log(ashcheck)
+    console.log(typeof ashcheck)
     
 //number to boolean=>
     let num5= 0
@@ -43,3 +43,13 @@ let name="ashish"
     console.log(typeof rollbool)
     //if we assign the value =0 and then convert the variable in to the boolean then it give flase and if i give the varriable 1 then the output is true and type is boolean 
  
+
+
+/*             *****  operation  *****             */
+
+/* in java script there are some type of operater 
+* assignment operater => = += -= *= /= **= %=
+* arithmetic operater => + - * / % ** ++ --
+* comparison operater => ++ <> 
+* bitwise operater => & | ^ 
+* type operater */
